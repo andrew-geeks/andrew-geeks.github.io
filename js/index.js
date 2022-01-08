603a996dd930c9efac328c97f42b9ba4b6cd5eb1
Andrew George Issac <andrewgeorgeissac@gmail.com>
2022-01-08 11:45:57 +0530

    Add files via upload

--- js/index.js
+++ js/index.js
@@ -5,13 +5,13 @@ console.log(year)
 
 
 document.querySelector(".n2-icon").addEventListener("click",()=>{
-    if(document.querySelector(".n2-block").style.height==="240px"){
+    if(document.querySelector(".n2-block").style.height==="280px"){
         
         document.querySelector(".n2-block").setAttribute("style","height:60px");
         document.querySelector(".n2-block .navigation-drop").setAttribute("style","display:none");
     }
     else{
-        document.querySelector(".n2-block").setAttribute("style","height:240px");
+        document.querySelector(".n2-block").setAttribute("style","height:280px");
         document.querySelector(".n2-block .navigation-drop").setAttribute("style","display:block");  
         
         
